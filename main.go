9fa6fc8fb813677619a398d9aee7a7546a67a128
25modad <25mordad@gmail.com>
2021-07-01 17:37:21 +0200

    test1 PASS

--- main.go
+++ main.go
@@ -2,11 +2,50 @@ package main
 
 import (
 	"fmt"
+	rcp "sales-tax/receipt"
 )
 
 ///
 func main() {
 
-	fmt.Println("START")
+	var p1 rcp.Product
+	p1.Quantity = 1
+	p1.Name = "bottle of perfume"
+	p1.Ptype = rcp.Other
+	p1.Price = 27.99
+	p1.IsImported = true
+	fmt.Println("p1", p1)
+
+	var p2 rcp.Product
+	p2.Quantity = 1
+	p2.Name = "perfume"
+	p2.Ptype = rcp.Other
+	p2.Price = 18.99
+	p2.IsImported = false
+	fmt.Println("p2", p2)
+
+	var p3 rcp.Product
+	p3.Quantity = 1
+	p3.Name = "packet of headache pills"
+	p3.Ptype = rcp.Medical
+	p3.Price = 9.75
+	p3.IsImported = false
+	fmt.Println("p3", p3)
+
+	var p4 rcp.Product
+	p4.Quantity = 1
+	p4.Name = "box of imported chocolates"
+	p4.Ptype = rcp.Food
+	p4.Price = 11.25
+	p4.IsImported = true
+	fmt.Println("p4", p4)
+
+	var products []rcp.Product
+	products = append(products, p1)
+	products = append(products, p2)
+	products = append(products, p3)
+	products = append(products, p4)
+	fmt.Printf("products %+v \n\n", products)
+	fmt.Printf("getReceipt %+v \n\n", rcp.GetReceipt(products))
 
 }
--- receipt/receipt.go
+++ receipt/receipt.go
@@ -1,5 +1,9 @@
 package receipt
 
+import (
+	"math"
+)
+
 type ProductType string
 
 const (
@@ -34,6 +38,61 @@ type Receipt struct {
 
 func GetReceipt(products []Product) Receipt {
 	var receipt Receipt
+	var receiptProduct ReceiptProduct
+	var receiptProducts []ReceiptProduct
+	var salesTax float64
+	var total float64
+	salesTax = 0
+	total = 0
+	for _, product := range products {
+		calculateTax(&product)
+		receiptProduct.quantity = product.Quantity
+		if product.IsImported {
+			receiptProduct.name = "Imported " + product.Name
+		} else {
+			receiptProduct.name = product.Name
+		}
+		receiptProduct.price = product.FinalPrice * float64(product.Quantity)
+		receiptProducts = append(receiptProducts, receiptProduct)
+		salesTax = salesTax + product.Tax*float64(product.Quantity)
+		total = total + product.FinalPrice*float64(product.Quantity)
+	}
+	receipt.products = receiptProducts
+	receipt.tax = round2Dec(salesTax)
+	receipt.total = round2Dec(total)
 
 	return receipt
+}
+
+func calculateTax(product *Product) {
+	product.TaxRate = getTaxRate(product)
+	product.Tax = ceil((float64(product.TaxRate)*product.Price)/100, 0.05)
+	product.FinalPrice = round2Dec(product.Price + product.Tax)
+}
+
+func getTaxRate(prd *Product) int64 {
+	var taxRate int64
+	taxRate = 0
+
+	if prd.Ptype == Other {
+		if prd.IsImported {
+			taxRate = 15
+		} else {
+			taxRate = 10
+		}
+
+	} else {
+		if prd.IsImported {
+			taxRate = 5
+		}
+	}
+	return taxRate
+}
+
+func ceil(x, unit float64) float64 {
+	return round2Dec((math.Ceil(x/unit) * unit))
+}
+
+func round2Dec(x float64) float64 {
+	return math.Round(x*100) / 100
 }
